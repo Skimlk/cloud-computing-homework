c7712124720fb7f4c036cddf6eb4bcc84f1c35de
Skim <vanhoutencocoapowder@gmail.com>
2026-09-16 18:21:07 -0400

    remove project dir

--- setup_venv.sh
+++ setup_venv.sh
@@ -5,6 +5,5 @@ source venv/bin/activate
 pip install -r requirements.txt
 
 SITE_PACKAGES=$(ls -d venv/lib/python*/site-packages | sort | tail -n 1)
-PROJECT_DIR=$(pwd)
 
-echo "$PROJECT_DIR" > "$SITE_PACKAGES/${PWD##*/}.pth" 
+echo "$(pwd)" > "$SITE_PACKAGES/${PWD##*/}.pth" 
